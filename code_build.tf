e32aa1b6f9b514ce1d45183b687bce9ec4e4cd73
victorhponcec <victorhponcec@gmail.com>
2025-11-07 15:05:10 -0300

    added: SG for DB | modified: codebuild repo url

--- code_build.tf
+++ code_build.tf
@@ -4,9 +4,9 @@ resource "aws_iam_role" "codebuild_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
+      Effect = "Allow"
       Principal = { Service = "codebuild.amazonaws.com" }
-      Action    = "sts:AssumeRole"
+      Action = "sts:AssumeRole"
     }]
   })
 }
@@ -16,22 +16,27 @@ resource "aws_iam_role_policy_attachment" "codebuild_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
 
+resource "aws_iam_role_policy_attachment" "codebuild_logs" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
 resource "aws_codebuild_project" "build_image" {
-  name         = "build-image"
-  service_role = aws_iam_role.codebuild_role.arn
+  name          = "build-image"
+  service_role  = aws_iam_role.codebuild_role.arn
   source {
     type      = "GITHUB"
-    location  = "https://github.com/victorhponcec/portfolio-eks-security.git"
+    location  = "https://github.com/victorhponcec/container-app-sec.git"
     buildspec = "buildspec.yml"
   }
 
   environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:7.0"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = true # Required for Docker
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true  # Required for Docker
     environment_variable {
-      name  = ECR_REPO
+      name = "ECR_REPO"
       value = aws_ecr_repository.app.repository_url
     }
   }
@@ -40,3 +45,8 @@ resource "aws_codebuild_project" "build_image" {
     type = "NO_ARTIFACTS"
   }
 }
+#to-do
+#integrate aws codebuild start-build --project-name build-image in terraform workflow, so it executes before the k8s deployment
+
+#aws codebuild start-build --project-name build-image
+#aws codebuild batch-get-builds --ids build-image:a53d82fb-6fae-43c5-b11a-c0a4e9a79ce3
